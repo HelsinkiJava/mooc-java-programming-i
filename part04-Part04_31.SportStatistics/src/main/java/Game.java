ca4caeb95c1850a241ef88c0601784118b346bf8
HelsinkiJava <gmaki@live.com>
2021-10-05 17:05:15 -0700

    Updates

---
+++ part04-Part04_31.SportStatistics/src/main/java/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+    private String team;
+    private int game;
+    public Game(String team, int game){
+        this.team = team;
+        this.game = game;
+    }
+    public String getTeam(){
+        return team;
+    }
+    public int getGame(){
+        return game;
+    }
+}
--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -1,4 +1,6 @@
 
+
+
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -7,7 +9,26 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("File:");
+        String file = scan.nextLine();
+        ArrayList<Game> info =readFile(file);
+        for(Game game: info){
+            System.out.println(game);
+        }
     }
 
+    public static ArrayList<Game> readFile(String file) {
+        ArrayList<Game> gamesPlayed = new ArrayList<>();
+        try(Scanner scanFile = new Scanner(Paths.get(file))){
+            while(scanFile.hasNextLine()){
+                String info[] = scanFile.nextLine().split(",");
+                String name = info[0];
+                int game = Integer.valueOf(info[1]);
+                gamesPlayed.add(new Game(name, game));
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return gamesPlayed;
+    }
 }
