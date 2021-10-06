c800b4df278191261923ada6a3d054f091ec4cd2
HelsinkiJava <gmaki@live.com>
2021-10-06 00:31:02 -0700

    df

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -8,27 +8,45 @@ import java.util.Scanner;
 public class SportStatistics {
 
     public static void main(String[] args) {
+        int game = 0;
+        int wins = 0;
+        int losses = 0;
         Scanner scan = new Scanner(System.in);
         System.out.println("File:");
         String file = scan.nextLine();
-        ArrayList<Game> info =readFile(file);
-        for(Game game: info){
-            System.out.println(game);
-        }
-    }
-
-    public static ArrayList<Game> readFile(String file) {
-        ArrayList<Game> gamesPlayed = new ArrayList<>();
-        try(Scanner scanFile = new Scanner(Paths.get(file))){
-            while(scanFile.hasNextLine()){
-                String info[] = scanFile.nextLine().split(",");
-                String name = info[0];
-                int game = Integer.valueOf(info[1]);
-                gamesPlayed.add(new Game(name, game));
+        System.out.println("Team:");
+        String team = scan.nextLine();
+        try (Scanner fileScan = new Scanner(Paths.get(file))) {
+            while (fileScan.hasNextLine()) {
+                String[] parts = fileScan.nextLine().split(",");
+                String home = parts[0].trim();
+                String away = parts[1].trim();
+                int pointsHome = Integer.valueOf(parts[2].trim());
+                int pointsAway = Integer.valueOf(parts[3].trim());
+                if (team.equals(home)) {
+                    game++;
+                    if (pointsHome < pointsAway) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
+                if (team.equals(away)) {
+                    game++;
+                    if (pointsAway > pointsHome) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
             }
-        }catch(Exception e){
+            System.out.println("Games: " + game);
+            System.out.println("Wins: " + wins);
+            System.out.println("Losses: " + losses);
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return gamesPlayed;
+
     }
 }
