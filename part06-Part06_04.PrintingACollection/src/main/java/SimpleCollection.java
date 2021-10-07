d9cf6994121309b5c1e0fa252aed7b10adb262e5
HelsinkiJava <gmaki@live.com>
2021-10-07 16:29:14 -0700

    First commit

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,24 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
+    @Override
+    public String toString(){
+        String listOfElements = "";
+        String output = "";
+        for(String e : elements){
+            if(e.equals(elements.get(elements.size() - 1))){
+               listOfElements += e;
+            }else{
+                listOfElements += e + "\n";
+            }
+        }
+        if(elements.size() == 1){
+            output = "The collection " + name + " has 1 element:\n" + listOfElements;
+        }else if(elements.size() == 0){
+            output = "The collection " + name + " is empty.";
+        }else
+            output = "The collection " + name + " has " + elements.size() + " elements:\n" + listOfElements;
+        return output;
+    }
     
 }
